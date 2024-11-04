255050fc289d889943d927fa0b3d03adfff6bfa2
MokSports596 <mokfantasysports@gmail.com>
2024-11-04 12:59:16 -0500

    updated link
    updated axios link

--- Frontend/components/axiosInstance.js
+++ Frontend/components/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://54.162.177.170:5062/api', // Replace with your EC2 public IP and backend port
+    baseURL: 'https://moksport-backend-dev.azurewebsites.net/api/', // Replace with your EC2 public IP and backend port
 });
 
 export default axiosInstance;
