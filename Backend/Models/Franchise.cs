303d7c586285a0f3e6641de648f7669c7560b9bd
Edelio Taboada <90776613+Edelio-Taboada@users.noreply.github.com>
2025-06-09 19:02:57 -0400

    Added new columns to Franchise table
    Added LoksLeft columns for corresponding to each team column. Also added HasLoad and SeasonPoints for each franchise. Updated Franchise model to reflect this. Created migrations to update DB.

--- Backend/Models/Franchise.cs
+++ Backend/Models/Franchise.cs
@@ -16,6 +16,14 @@ namespace MokSportsApp.Models
         public int? Team4Id { get; set; }
         public int? Team5Id { get; set; }
 
+        // Current peformance statistics for the franchise
+        public int? Team1LoksLeft { get; set; }
+        public int? Team2LoksLeft { get; set; }
+        public int? Team3LoksLeft { get; set; }
+        public int? Team4LoksLeft { get; set; }
+        public int? Team5LoksLeft { get; set; }
+        public bool? HasLoad { get; set; }
+        public int? SeasonPoints { get; set; }
         // Navigation properties
         public User User { get; set; }
         public League League { get; set; }
